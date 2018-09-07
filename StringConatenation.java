431bb6f7c0ab57f90d6f4352c8064f6bf009bf34
Swetabh Suman <swetabhsuman8@gmail.com>
2018-09-07 08:29:51 +0530

    Add files via upload

---
+++ StringConatenation.java
@@ -0,0 +1,7 @@
+public class StringConatenation {
+	public static void main (String [] args) {
+		String hello = "Hello, how are you ";
+		String name = "Swetabh";
+		System.out.println (hello + name); //String Conatension
+	}
+}
