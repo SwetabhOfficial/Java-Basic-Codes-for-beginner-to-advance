dbbe2b12a1794599f99d09a73b1c9400b7b63940
Swetabh Suman (GitCodeSwetabh) <swetabhsuman8@gmail.com>
2018-09-12 00:06:39 +0530

    Delete StringConatenation.java

--- StringConatenation.java
+++
@@ -1,7 +0,0 @@
-public class StringConatenation {
-	public static void main (String [] args) {
-		String hello = "Hello, how are you ";
-		String name = "Swetabh";
-		System.out.println (hello + name); //String Conatension
-	}
-}
